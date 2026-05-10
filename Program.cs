f23eadc1fa9099a679c5d4c26cd0c10f8e3671df
shirefyassen7-lab <shirefyassen7@gmail.com>
2026-05-10 12:47:40 +0300

    test

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System.ComponentModel.Design;
+using System.Diagnostics;
+using System.Threading.Channels;
+
+namespace session07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int secret = 66;
+            int guessleft = 3;
+
+
+            while (guessleft > 0)
+            {
+                Console.WriteLine($"guess the secret {guessleft} tries left : ");
+                int guess = Convert.ToInt32(Console.ReadLine());
+
+                if (guess == secret)
+                {
+                    Console.WriteLine("its true");
+                    break;
+                }
+                else if (guess < secret)
+                {
+                    Console.WriteLine("too low");
+                }
+                else
+                    Console.WriteLine("too high");
+
+                guessleft--;
+            }
+            if (guessleft == 0)
+                Console.WriteLine("game over");
+
+
+
+
+
+
+            string massage = null;
+            Console.WriteLine(massage?.Length);
+
+            for (int i = 1; i <= 20; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                    Console.WriteLine("fizzbuzz");
+                else if (i % 3 == 0)
+                    Console.WriteLine("fizz");
+                else if (i % 5 == 0)
+                    Console.WriteLine("buzz");
+                else Console.WriteLine(i);
+            }
+
+
+        }
+    }
+}
